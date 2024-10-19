b26054f91d84572121b05abee1c17e7eed868798
Zülal Cengiz <zc.swe@gmail.com>
2024-10-19 17:14:22 +0300

    signup fixes

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -4,29 +4,15 @@ import React, { useEffect } from "react";
 import Navbar from "@/app/(components)/Navbar";
 import Sidebar from "./(components)/Sidebar";
 import StoreProvider, { useAppDispatch, useAppSelector } from "./redux";
-import { usePathname, useRouter } from "next/navigation";
-import { setToken } from "@/state/authSlice";
 import AuthProvider from "./authProvider";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const dispatch = useAppDispatch();
   const isSidebarOpen = useAppSelector((state) => state.global.isSidebarOpen);
-  const pathname = usePathname();
-  const isLoginPage = pathname === "/";
-  const isAuthenticated = useAppSelector((state) => state.auth.isAuthenticated);
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      dispatch(setToken(token));
-    }
-  }, [dispatch]);
 
   return (
     <div
-      className={`${
-        !isLoginPage && isAuthenticated
-          ? "light flex bg-gray-50 text-gray-900 w-full min-h-screen "
+      className={`"light flex bg-gray-50 text-gray-900 w-full min-h-screen "
           : ""
       }`}
     >
@@ -44,14 +30,6 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 };
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
-  const router = useRouter();
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (!token) {
-      router.push("/login");
-    }
-  }, [router]);
   return (
     <StoreProvider>
       <AuthProvider>
